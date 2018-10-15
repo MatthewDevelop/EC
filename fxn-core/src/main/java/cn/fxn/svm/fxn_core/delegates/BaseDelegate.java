d83efcb876c37049fde0a02b9d1120f9a119ff1e
Matthew <swd-004@mail.foxconn.com>
2018-10-15 11:15:39 +0800

    在BaseDelegate中添加视图绑定功能

--- fxn-core/src/main/java/cn/fxn/svm/fxn_core/delegates/BaseDelegate.java
+++ fxn-core/src/main/java/cn/fxn/svm/fxn_core/delegates/BaseDelegate.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import me.yokeyword.fragmentation_swipeback.SwipeBackFragment;
 
 /**
@@ -16,11 +18,34 @@ import me.yokeyword.fragmentation_swipeback.SwipeBackFragment;
  * @func:
  */
 public abstract class BaseDelegate extends SwipeBackFragment {
-    public abstract Object setLayout();
+    
+    private Unbinder mUnbinder = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = null;
+        if (setLayout() instanceof Integer) {
+            rootView = inflater.inflate((Integer) setLayout(), container, false);
+        } else if (setLayout() instanceof View) {
+            rootView = (View) setLayout();
+        }
+        if (rootView != null) {
+            mUnbinder = ButterKnife.bind(this, rootView);
+            onBindView(savedInstanceState, rootView);
+        }
+        return rootView;
+    }
+
+    public abstract Object setLayout();
+
+    public abstract void onBindView(@Nullable Bundle savedInstanceState, View rootView);
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mUnbinder != null) {
+            mUnbinder.unbind();
+        }
     }
 }
