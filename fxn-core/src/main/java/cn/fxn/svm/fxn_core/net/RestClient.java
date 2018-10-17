7f366f08183302efa1b4f09a161f87999b88dac2
Matthew <swd-004@mail.foxconn.com>
2018-10-17 11:01:19 +0800

    完善网络框架
    添加上传功能

--- fxn-core/src/main/java/cn/fxn/svm/fxn_core/net/RestClient.java
+++ fxn-core/src/main/java/cn/fxn/svm/fxn_core/net/RestClient.java
@@ -2,6 +2,7 @@ package cn.fxn.svm.fxn_core.net;
 
 import android.content.Context;
 
+import java.io.File;
 import java.util.WeakHashMap;
 
 import cn.fxn.svm.fxn_core.net.callback.IError;
@@ -11,6 +12,8 @@ import cn.fxn.svm.fxn_core.net.callback.ISuccess;
 import cn.fxn.svm.fxn_core.net.callback.RequestCallbacks;
 import cn.fxn.svm.fxn_core.ui.EcLoader;
 import cn.fxn.svm.fxn_core.ui.LoaderStyle;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,14 +33,16 @@ public class RestClient {
     private final IError ERROR;
     private final RequestBody BODY;
     private final LoaderStyle LOADER_STYLE;
+    private final File FILE;
     private final Context CONTEXT;
 
     public RestClient(String url, WeakHashMap<String, Object> params,
                       IRequest request, ISuccess success,
                       IFailure failure, IError error,
-                      RequestBody body, LoaderStyle loaderStyle, Context context) {
+                      RequestBody body, LoaderStyle loaderStyle, File file, Context context) {
         this.URL = url;
         this.LOADER_STYLE = loaderStyle;
+        this.FILE = file;
         this.CONTEXT = context;
         PARAMS.putAll(params);
         this.REQUEST = request;
@@ -72,11 +77,22 @@ public class RestClient {
             case POST:
                 call = restService.post(URL, PARAMS);
                 break;
+            case POST_RAW:
+                call = restService.postRaw(URL, BODY);
+                break;
             case PUT:
                 call = restService.put(URL, PARAMS);
                 break;
+            case PUT_RAW:
+                call = restService.putRaw(URL, BODY);
+                break;
             case DELETE:
                 call = restService.delete(URL, PARAMS);
+                break;
+            case UPLOAD:
+                final RequestBody requestBody=RequestBody.create(MediaType.parse(MultipartBody.FORM.toString()), FILE);
+                final MultipartBody.Part body=MultipartBody.Part.createFormData("file",FILE.getName(),requestBody);
+                call=restService.upload(URL, body);
                 break;
             default:
                 break;
@@ -91,11 +107,26 @@ public class RestClient {
     }
 
     public final void post() {
-        request(HttpMethod.POST);
+        if (BODY == null) {
+            request(HttpMethod.POST);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("PARAMS must be null!");
+            }
+            request(HttpMethod.POST_RAW);
+        }
     }
 
+
     public final void put() {
-        request(HttpMethod.PUT);
+        if (BODY == null) {
+            request(HttpMethod.PUT);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("PARAMS must be null!");
+            }
+            request(HttpMethod.PUT_RAW);
+        }
     }
 
     public final void delete() {
--- fxn-core/src/main/java/cn/fxn/svm/fxn_core/net/RestClientBuilder.java
+++ fxn-core/src/main/java/cn/fxn/svm/fxn_core/net/RestClientBuilder.java
@@ -2,6 +2,7 @@ package cn.fxn.svm.fxn_core.net;
 
 import android.content.Context;
 
+import java.io.File;
 import java.util.WeakHashMap;
 
 import cn.fxn.svm.fxn_core.net.callback.IError;
@@ -20,74 +21,85 @@ import okhttp3.RequestBody;
  */
 public class RestClientBuilder {
 
-    private String mUrl;
-    private static final WeakHashMap<String, Object> PARAMS=RestCreator.getParams();
-    private IRequest mIRequest;
-    private ISuccess mISuccess;
-    private IFailure mIFailure;
-    private IError mIError;
-    private RequestBody mBody;
-    private Context mContext;
-    private LoaderStyle mLoaderStyle;
+    private static final WeakHashMap<String, Object> PARAMS = RestCreator.getParams();
+    private String mUrl = null;
+    private IRequest mIRequest = null;
+    private ISuccess mISuccess = null;
+    private IFailure mIFailure = null;
+    private IError mIError = null;
+    private RequestBody mBody = null;
+    private Context mContext = null;
+    private LoaderStyle mLoaderStyle = null;
+    private File mFile = null;
 
-    RestClientBuilder(){
+    RestClientBuilder() {
 
     }
 
-    public final RestClientBuilder url(String url){
-        mUrl=url;
+    public final RestClientBuilder url(String url) {
+        mUrl = url;
         return this;
     }
 
-    public final RestClientBuilder params(WeakHashMap<String,Object> params){
+    public final RestClientBuilder params(WeakHashMap<String, Object> params) {
         PARAMS.putAll(params);
         return this;
     }
 
-    public final RestClientBuilder params(String key,Object value){
+    public final RestClientBuilder params(String key, Object value) {
         PARAMS.put(key, value);
         return this;
     }
 
-    public final RestClientBuilder raws(String raws){
-        mBody=RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), raws);
+    public final RestClientBuilder file(File file) {
+        mFile = file;
         return this;
     }
 
-    public final RestClientBuilder onRequest(IRequest iRequest){
-        mIRequest=iRequest;
+    public final RestClientBuilder file(String filePath) {
+        mFile = new File(filePath);
         return this;
     }
 
-    public final RestClientBuilder success(ISuccess iSuccess){
-        mISuccess=iSuccess;
+    public final RestClientBuilder raws(String raws) {
+        mBody = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), raws);
         return this;
     }
 
-    public final RestClientBuilder failure(IFailure iFailure){
-        mIFailure=iFailure;
+    public final RestClientBuilder onRequest(IRequest iRequest) {
+        mIRequest = iRequest;
         return this;
     }
 
-    public final RestClientBuilder error(IError iError){
-        mIError=iError;
+    public final RestClientBuilder success(ISuccess iSuccess) {
+        mISuccess = iSuccess;
         return this;
     }
 
-    public final RestClientBuilder loader(Context context,LoaderStyle loaderStyle){
-        mContext=context;
-        mLoaderStyle=loaderStyle;
+    public final RestClientBuilder failure(IFailure iFailure) {
+        mIFailure = iFailure;
         return this;
     }
 
-    public final RestClientBuilder loader(Context context){
-        mContext=context;
-        mLoaderStyle=LoaderStyle.BallClipRotatePulseIndicator;
+    public final RestClientBuilder error(IError iError) {
+        mIError = iError;
         return this;
     }
 
-    public final RestClient build(){
+    public final RestClientBuilder loader(Context context, LoaderStyle loaderStyle) {
+        mContext = context;
+        mLoaderStyle = loaderStyle;
+        return this;
+    }
+
+    public final RestClientBuilder loader(Context context) {
+        mContext = context;
+        mLoaderStyle = LoaderStyle.BallClipRotatePulseIndicator;
+        return this;
+    }
+
+    public final RestClient build() {
         return new RestClient(mUrl, PARAMS, mIRequest,
-                mISuccess, mIFailure, mIError, mBody, mLoaderStyle, mContext);
+                mISuccess, mIFailure, mIError, mBody, mLoaderStyle, mFile, mContext);
     }
 }
