43655835b058f78e605e688dbfd9b96255343cbd
Matthew <swd-004@mail.foxconn.com>
2018-11-02 09:19:06 +0800

    不同类别订单增加订单标题

--- fxn-ec/src/main/java/cn/fxn/svm/fxn_ec/main/user/order/OrderListDelegate.java
+++ fxn-ec/src/main/java/cn/fxn/svm/fxn_ec/main/user/order/OrderListDelegate.java
@@ -2,6 +2,7 @@ package cn.fxn.svm.fxn_ec.main.user.order;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -29,8 +30,11 @@ public class OrderListDelegate extends EcDelegate {
 
     @BindView(R2.id.rv_order_list)
     RecyclerView mRecyclerView = null;
+    @BindView(R2.id.tv_order_list_type)
+    AppCompatTextView tvOrderTitle;
     private String mType = null;
     private String mUrl = null;
+    private Bundle mArgs;
 
     @Override
     public Object setLayout() {
@@ -39,38 +43,41 @@ public class OrderListDelegate extends EcDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
-
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        final Bundle args = getArguments();
-        if (args != null) {
-            mType = args.getString(UserDelegate.ORDER_TYPE);
+        if (mArgs != null) {
+            mType = mArgs.getString(UserDelegate.ORDER_TYPE);
             if (mType != null) {
                 switch (mType) {
                     case UserDelegate.ALL:
                         mUrl = "order_list_all.json";
+                        tvOrderTitle.setText("全部订单");
                         break;
                     case UserDelegate.PAY:
                         mUrl = "order_list_pay.json";
+                        tvOrderTitle.setText("待付款");
                         break;
                     case UserDelegate.RECEIVE:
                         mUrl = "order_list_receive.json";
+                        tvOrderTitle.setText("待收货");
                         break;
                     case UserDelegate.EVALUATE:
                         mUrl = "order_list_evaluate.json";
+                        tvOrderTitle.setText("待评价");
                         break;
                     case UserDelegate.AFTER_SALE:
                         mUrl = "order_list_after_sale.json";
+                        tvOrderTitle.setText("售后");
                         break;
                     default:
                         break;
                 }
             }
         }
+    }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mArgs = getArguments();
     }
 
     @Override
